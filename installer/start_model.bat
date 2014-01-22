2d734e5d30352081a8f9b197e2ce78b3dde0bda5
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-01-22 23:16:38 +0100

    trying to fix installer, drawback of migrating from rs 2.0

--- installer/start_model.bat
+++ installer/start_model.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-TITLE jzombie
+TITLE meetingPlace
 
 REM Repast Simphony Model Starter
 REM By Michael J. North
@@ -28,7 +28,7 @@ SET CP=%CP%;%REPAST_SIMPHONY_LIB%commons-cli-1.2.jar
 SET CP=%CP%;../groovylib/$Groovy_All_Jar
 
 REM Change to the Default Repast Simphony Directory
-CD jzombie
+CD meetingPlace
 
 REM Start the Model
-START javaw -Xss10M -Xmx400M -cp %CP% repast.simphony.runtime.RepastMain ./jzombie.rs
+START javaw -Xss10M -Xmx400M -cp %CP% repast.simphony.runtime.RepastMain ./meetingPlace.rs
